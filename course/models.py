6d5e1f397d0bdf090e5998122e381483a4d19717
sina hozhabri <sinahzdev@gmail.com>
2025-04-29 17:30:55 +0330

    making some methods more general

--- course/models.py
+++ course/models.py
@@ -23,14 +23,45 @@ def gen_public_id(instance):
         return unique_id
     slug = slugify(title)
     unique_id_short = unique_id[:5]
-    return f'{title}-{unique_id_short}'
+    return f'{slug}-{unique_id_short}'
+
+def get_public_id_prefix(instance):
+    if hasattr(instance,'path'):
+        path = instance.path
+        if path.startswith('/'):
+            path = path[1:]
+        if path.endswith('/'):
+            path = path[:-1]
+        return path
+    public_id = instance.public_id
+    instance_class = instance.__class__
+    instance_class_name = instance_class.__name__
+    model_name_slug = slugify(instance_class_name)
+    if not public_id:
+        return f'{model_name_slug}'
+    return f'{model_name_slug}/{public_id}'
 
 
+def get_display_name(instance):
+    if hasattr(instance,'get_display_name'):
+        return instance.get_display_name()
+    elif hasattr(instance,'title'):
+        return instance.title
+    instance_class = instance.__class__
+    instance_class_name = instance_class.__name__
+    return f'{instance_class_name}-upload'
 class CourseModel(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True,null=True)
-    public_id = models.CharField(max_length=130)
-    image = CloudinaryField('image',blank=True,null=True,public_id_prefix=)
+    public_id = models.CharField(max_length=130,blank=True,null=True)
+    image = CloudinaryField('image',blank=True,null=True,
+                            
+                            public_id_prefix=get_public_id_prefix,
+
+                            display_name=get_display_name,
+
+                            tags=['course','thumbnail'])
+    
     access = models.CharField(max_length=5,choices=AccessRequirements.choices,default=AccessRequirements.EMAIL_REQUIRED)
     status = models.CharField(max_length=7,choices=PublishStatus.choices,default=PublishStatus.DRAFT)
     @property
@@ -44,27 +75,43 @@ class CourseModel(models.Model):
         }
         url = self.image.build_url(**image_options)
         return url
-    def get_image_thumbnail(self,as_html=False,width=500):
-        if not self.image:
-            return ""
-        image_options = {
-            'width':width
-        }
-        if as_html:
-            return self.image.image(**image_options)
-        url = self.image.build_url(**image_options)
-        return url
-    def save(self):
+  
+    def get_display_name(self):
+
+        return f'{self.title}-course'
+    @property
+    def path(self):
+        return f'/course/{self.public_id}'
+    
+    def get_absolute_url(self):
+        return self.path
+    
+
+
+    def save(self,*args, **kwargs):
         if self.public_id == '' or self.public_id == None:
             self.public_id = gen_public_id(self)
-        super().save()
+        super().save(*args, **kwargs)
     
 class lessonModel(models.Model):
     course = models.ForeignKey(CourseModel,on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     description = models.TextField()
-    image = CloudinaryField('image',blank=True,null=True,)
-    video = CloudinaryField('video',blank=True,null=True,resource_type='video')
+    public_id = models.CharField(max_length=130,blank=True,null=True)
+    image = CloudinaryField('image',blank=True,null=True,
+                            
+                            public_id_prefix=get_public_id_prefix,
+
+                            display_name=get_display_name,
+                            
+                            tags=['lesson','thumbnail'])
+    
+    video = CloudinaryField('video',blank=True,null=True,resource_type='video',
+                            
+                            public_id_prefix=get_public_id_prefix,
+
+                            display_name=get_display_name)
+    
     can_preview = models.BooleanField(default=False,help_text='if user do not have access to course can they see this?')
     status = models.CharField(max_length=7,choices=PublishStatus.choices,default=PublishStatus.DRAFT)
     ordering = models.IntegerField(default=0)
@@ -72,3 +119,21 @@ class lessonModel(models.Model):
     datetime_updated = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ['ordering','-datetime_updated']
+    
+    def save(self,*args, **kwargs):
+        if self.public_id == '' or self.public_id == None:
+            self.public_id = gen_public_id(self)
+        super().save(*args, **kwargs)
+
+    def get_display_name(self):
+
+        return f'{self.title}-{self.course.get_display_name()}'
+    @property
+    def path(self):
+        course_path = self.course.path
+        if course_path.endswith('/'):
+            course_path = course_path[:-1]
+        return f'{course_path}/lessons/{self.public_id}'
+    
+    def get_absolute_url(self):
+        return self.path
