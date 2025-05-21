bbb725629581c43e5ea412373d3c95f5f52f32f2
sina hozhabri <sinahzdev@gmail.com>
2025-05-21 03:44:27 +0330

    add course list subject path

--- course/urls.py
+++ course/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('',views.course_list_view,name='course-list'),
+    path('<str:subject>/',views.course_list_subject_view,name='course-list-subject'),
     path('<slug:course_public_id>/',views.course_detail_view,name='course-detail'),
     path('<slug:course_public_id>/lessons/<slug:lesson_public_id>',views.lesson_detail_view,name='lesson-detail')
 
